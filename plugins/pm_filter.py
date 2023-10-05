2afc07c2bd9ab7af7f5301033a149d05958de0d1
Jagadeeshgowdayt <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2023-10-05 16:20:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2045,20 +2045,19 @@ async def auto_filter(client, msg, spoll=False):
                         await message.delete()
                     else:
                         fuk = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
-    
-    await m.delete()
-    try:
-        if settings['auto_delete']:
-            await asyncio.sleep(300)
-            await fuk.delete()
-            await message.delete()
-    except KeyError:
-        await save_group_settings(message.chat.id, 'auto_delete', True)
-        await asyncio.sleep(300)
-        await fuk.delete()
-        await message.delete()
-    # if spoll:
-    #     await msg.message.delete()
+                        await m.delete()
+                        try:
+                            if settings['auto_delete']:
+                                await asyncio.sleep(300)
+                                await fuk.delete()
+                                await message.delete()
+                        except KeyError:
+                            await save_group_settings(message.chat.id, 'auto_delete', True)
+                            await asyncio.sleep(300)
+                            await fuk.delete()
+                            await message.delete()
+                            # if spoll:
+#     await msg.message.delete()
 
 
 async def advantage_spell_chok(client, msg):
